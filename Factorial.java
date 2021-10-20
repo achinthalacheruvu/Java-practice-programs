17dc2e0f7d473284f97830232c01c2003fcfdf7d
achinthalacheruvu <anudasmartgal@yahoo.com>
2021-10-20 00:27:01 -0700

    Adding Factorial program

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {  
+    static void factorial() {
+    	int fact = 1;
+        for(int a=1; a<= 10; a++) {
+        	fact *= a;
+        }
+        System.out.println(fact);
+    }
+    
+    public static void main(String[] args) {
+    factorial();
+    }
+}
