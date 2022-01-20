040c4c14e3d586d9b19c39f4be42741271777cfa
achinthalacheruvu <anudasmartgal@yahoo.com>
2022-01-19 20:38:12 -0800

    Adding program to print characters from string

---
+++ PrintingCharactersFromString.java
@@ -0,0 +1,10 @@
+public class PrintingCharactersFromString{
+
+    public static void main(String[] args) {
+         String str = "Greedy Algorithms";
+         for(int i=0; i<str.length(); i++){
+             System.out.println("Characters in string are: " + str.charAt(i));
+            }
+        
+    }
+}
